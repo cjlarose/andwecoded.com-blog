4eb5d79437f608e2beefa278717ab9e9ecd0a0b8
root <homeski@cox.net>
2013-11-03 15:15:32 -0500

    Added en locales and more layout changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-  	@posts = Post.all
+  	@posts = Post.all.order('created_at DESC');
   end
 end
