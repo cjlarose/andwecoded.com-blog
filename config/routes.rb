821b96c62bb1eb5fea8a7d4f29aa17f7c2e63755
root <homeski@cox.net>
2014-01-02 00:37:40 -0500

    Made changes to portfolio page

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Blog::Application.routes.draw do
 
   resources :topics
 
+  get 'projects/' => 'projects#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
